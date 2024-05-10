a36006aa98e9fa7143b2d266b92357cfd2e1fa22
Cosmin Trusca <mariancosmintrusca@gmail.com>
2024-05-10 19:36:26 +0000

    Add some questions

---
+++ assets/js/script.js
@@ -0,0 +1,62 @@
+// Questions
+let questions [
+    {
+        question: "What is the capital of England?";
+        answers: [
+            {text: Paris; correct: false};
+            {text: London; correct: true};
+            {text: Bucharest; correct: false};
+            {text: Vienna; correct: false};
+        ]
+    };
+    {
+        question: "What is the largest planet in our solar sistem?";
+        answers: [
+            {text: Earth; correct: false};
+            {text: Saturn; correct: false};
+            {text: Mars; correct: false};
+            {text: Jupiter; correct: true};
+        ]
+    };
+    {
+        question: "2+2?";
+        answers: [
+            {text: 4; correct: true};
+            {text: 3; correct: false};
+            {text: 6; correct: false};
+            {text: 22; correct: false};
+        ]
+    };
+    {
+        question: "How many time zones are there in Russia?";
+        answers: [
+            {text: 23; correct: false};
+            {text: 11; correct: true};
+            {text: 12; correct: false};
+            {text: 33; correct: false};
+        ]
+    };
+    {
+        question: "How many stripes are there on the US flag?";
+        answers: [
+            {text: 13; correct: true};
+            {text: 23; correct: false};
+            {text: 44; correct: false};
+            {text: 12; correct: false};
+        ]
+    };
+    {
+        question: "How many days does it take for the Earth to orbit the Sun?";
+        answers: [
+            {text: 256; correct: false};
+            {text: 30; correct: false};
+            {text: 7; correct: false};
+            {text: 365; correct: true};
+            ]
+    };
+    {
+        
+    }
+
+    
+]
